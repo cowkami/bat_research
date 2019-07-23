2b372232e48055a7e6145a5dd2cd16280054c9b6
lee <lee.unseo17@gmail.com>
2019-07-23 16:45:41 +0900

    add constants.py

---
+++ libs/constants.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+data_path = Path('/home/lee/bat_research/data')
+
+raw_path = data_path / 'raw'
+processed_path = data_path / 'processed'
+interim_path = data_path / 'interim'
+
+raw_190526_path = raw_path / '190526'
+pross_190526_path = processed_path / '190526'
+
